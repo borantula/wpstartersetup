ee9b5bc672f62a1ab3b24768afc782599cf9f05a
Bora Yalcin <email@borayalcin.me>
2014-04-16 22:07:18 +0300

    wp 3.9 update

--- wp-content/plugins/itro-popup/mc-main.php
+++ wp-content/plugins/itro-popup/mc-main.php
@@ -6,14 +6,14 @@ This file is part of ITRO Popup Plugin.
 Plugin Name: ITRO Popup Plugin
 Plugin URI: http://www.itro.eu/
 Description: EN - Show a perfecly centered customizable popup and a popup-system for age-restricted site and allow to insert own HTML code. IT - Visualizza un popup perfettamente centrato e personalizzabile con possibile blocco per i siti con restrizioni di eta' e permette di inserire il proprio codice HTML.
-Author: I.T.RO.(c) Sez. Informatica
+Author: ITRO Team
 E-mail: support@itro.eu
-Version: 4.6
+Version: 4.6.1
 Author URI: http://www.itro.eu/
 */
 
 global $ITRO_VER;
-$ITRO_VER = 4.6;
+$ITRO_VER = 4.61;
 
 define('itroPath', plugins_url() . '/itro-popup/');
 define('itroImages', plugins_url() . '/itro-popup/images/');
