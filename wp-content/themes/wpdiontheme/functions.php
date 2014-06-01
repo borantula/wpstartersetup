56c93e3fb78e811184e52bc8118e688541af3095
Bora Yalcin <email@borayalcin.me>
2014-06-01 12:52:41 +0300

    typo fixed

--- wp-content/themes/wpdiontheme/functions.php
+++ wp-content/themes/wpdiontheme/functions.php
@@ -1,21 +1,23 @@
-bo<?php
+<?php
 /**
 * theme constants
 */
+
 /*
 * Lowercase theme slug which is also theme directory
 */
+
 define('DION_THEME_SLUG','wpdion');
 define('DION_THEME_URL',get_stylesheet_directory_uri());
 define('DION_THEME_DIR',get_stylesheet_directory());
 
 define('DION_COMPONENTS_URL',get_stylesheet_directory_uri().'/components');
 
-if ( ! isset( $content_width ) )
+if ( ! isset( $content_width ) ) {
 	$content_width = 640; /* pixels */
+}
 
 require DION_THEME_DIR.'/inc/vendor/autoload.php';
-
 
 //setting up the theme
 Dion\ThemeSetup::getInstance();
